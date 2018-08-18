252f2659eda79b0affd79ca3e38ba610657ded58
Ian Denhardt <ian@zenhack.net>
2018-08-18 17:30:05 -0400

    Update bitrotten error message

--- main.go
+++ main.go
@@ -124,7 +124,7 @@ func main() {
 	flag.Parse()
 
 	if *imageName == "" {
-		usageErr("Missing option: -image")
+		usageErr("Missing option: -imagefile")
 	}
 
 	pkgDefParts := strings.SplitN(*pkgDef, ":", 2)
